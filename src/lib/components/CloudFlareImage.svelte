72f4cc48fa4a5226997c174f6c339237e3b12b65
ikealmighty <isaacyates7@gmail.com>
2024-05-30 12:40:46 -0500

    added CloudFlareImage that shows an image from cloudflare cdn

---
+++ src/lib/components/CloudFlareImage.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { PUBLIC_CLOUDFLARE_URL } from '$env/static/public';
+
+	export let src;
+	export let alt;
+
+	const fullURL = `${PUBLIC_CLOUDFLARE_URL}/${src}`;
+</script>
+
+<img src={fullURL} {alt} />
+
+<style>
+	img {
+		width: 100%;
+	}
+</style>
